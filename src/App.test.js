48855d46e5c4726425c4f50622512551534ee7cd
eternale88 <ethanrobbins@ymail.com>
2021-08-01 14:45:03 -0400

    add and finish all fusionCharts

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,84 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { GithubContext, GithubProvider } from './context/context'
+import mockUser from './context/mockData.js/mockUser'
+import mockRepos from './context/mockData.js/mockRepos'
+import TestRenderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// const chartData = [
+//   {
+//     label: "HTML",
+//     value: "13"
+//   },
+//   {
+//     label: "CSS",
+//     value: "23"
+//   },
+//   {
+//     label: "JavaScript",
+//     value: "80"
+//   }
+// ]
+
+// const fakeFollowers = [
+//   {
+//   "login": "capitalist",
+//   "id": 1639,
+//   "node_id": "MDQ6VXNlcjE2Mzk=",
+//   "avatar_url": "https://avatars.githubusercontent.com/u/1639?v=4",
+//   "gravatar_id": "",
+//   "url": "https://api.github.com/users/capitalist",
+//   "html_url": "https://github.com/capitalist",
+//   "followers_url": "https://api.github.com/users/capitalist/followers",
+//   "following_url": "https://api.github.com/users/capitalist/following{/other_user}",
+//   "gists_url": "https://api.github.com/users/capitalist/gists{/gist_id}",
+//   "starred_url": "https://api.github.com/users/capitalist/starred{/owner}{/repo}",
+//   "subscriptions_url": "https://api.github.com/users/capitalist/subscriptions",
+//   "organizations_url": "https://api.github.com/users/capitalist/orgs",
+//   "repos_url": "https://api.github.com/users/capitalist/repos",
+//   "events_url": "https://api.github.com/users/capitalist/events{/privacy}",
+//   "received_events_url": "https://api.github.com/users/capitalist/received_events",
+//   "type": "User",
+//   "site_admin": false
+//   }
+// ]
+
+// const MockApp = () => {
+//   return (
+//       <BrowserRouter>
+//           <App />
+//       </BrowserRouter>
+//   )
+// }
+
+// const customRender = (ui, {providerProps, ...renderOptions}) => {
+//   return render(
+//     <GithubContext.Provider {...providerProps}>{ui}</GithubContext.Provider>,
+//     renderOptions,
+//   )
+// }
+// const repos = chartData
+// test('renders learn react link', () => {
+//   const providerProps = {
+//     mockUser
+//   }
+//   customRender(<MockApp/>, {providerProps}
+  
+//     // <GithubContext.Provider value={{mockUser,repos, fakeFollowers}}>
+//     //             <MockApp/>
+//     // </GithubContext.Provider>
+
+    
+//   )
+//   // const element = new TestRenderer.create(
+//   //   					<GithubContext.Provider value={{repos}}>
+//   //               <MockApp/>
+//   //             </GithubContext.Provider>
+//   //             )
+//   //const { getByText } = render(<MockApp />);
+//   // const linkElement = getByText(/learn react/i);
+//   // expect(linkElement).toBeInTheDocument();
+// })
+
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,19 +6,23 @@ const Repos = () => {
   const {repos} = React.useContext(GithubContext)
   // use reduce to get languages of each repo, 
   // and for each language add it to our total and return the total
-  let languages = repos.reduce((total, item) => {
-    const {language} = item // pull lang out of each repo
+  const languages = repos.reduce((total, item) => {
+    const {language, stargazers_count} = item // pull lang out of each repo
     //if language is not false return languages
     if(!language) return total // means don't do anything to languages that are null
 
     //if total has no value (if lang isn't on obj) create obj, change its value prop to 1
     // if it already has value(on obj) change increment its value prop, /////char map
     if(!total[language]) {
-      total[language] = {label: language, value:1}
+      total[language] = {label: language, value:1, 
+        stars: stargazers_count
+      }
     } else {
       total[language] = {
         ...total[language],
-         value: total[language].value + 1 }
+         value: total[language].value + 1,
+         stars: total[language].stars + stargazers_count
+        }
     }
       return total //must return total or reduce won't work
   }, {}) // {} - means that our total starts out as empty obj
@@ -41,36 +45,71 @@ const Repos = () => {
 
 // then we sort them so most popular languages display first,
 // as we'll remove the others
-languages = Object.values(languages)
+//mostUsed - languages
+
+const mostUsed = Object.values(languages)
 .sort((a, b) => {
   return b.value - a.value
 })
 .slice(0, 5)
 
+// mostStars per language
+const mostStars = Object.values(languages)
+.sort((a, b) => {
+  return b.stars - a.stars
+}).map((item) => {
+  //reassigning stars to the value prop, have to do this as value prop is what chart looks for, and value is currently setup for languages, this is for starred repos
+  return {...item, value: item.stars}
+})
+.slice(0, 5)
+
+//starred repos, and most forked repos
+//label and value props are what fusionChars expects
+let {stars, forks} = repos.reduce((total, item) => {
+  const{ stargazers_count, name, forks } = item
+  total.stars[stargazers_count] = {label: name,
+    value: stargazers_count
+  }
+  total.forks[forks] = {label: name,
+    value: forks
+  }
+ return total
+}, { stars: {}, forks: {}})
+
+//convert array of objs
+stars = Object.values(stars)
+.slice(0, 5)
+.reverse()
+
+forks = Object.values(forks)
+.slice(0, 5)
+.reverse()
 
 
-  //console.log(languages)
   //example chart data
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13"
-    },
-    {
-      label: "CSS",
-      value: "23"
-    },
-    {
-      label: "JavaScript",
-      value: "80"
-    }
-  ]
+  // const chartData = [
+  //   {
+  //     label: "HTML",
+  //     value: "13"
+  //   },
+  //   {
+  //     label: "CSS",
+  //     value: "23"
+  //   },
+  //   {
+  //     label: "JavaScript",
+  //     value: "80"
+  //   }
+  // ]
   return (
     <section className="section">
       <Wrapper className="section-center">
       {/*Example chart for reference*/}
       {/* <ExampleChart data={chartData}/>*/}
-      <Pie3D data={languages}/>
+      <Pie3D data={mostUsed}/>
+      <Column3D data={stars}/>
+      <Doughnut2D data={mostStars}/>
+      <Bar3D data={forks}/>
       </Wrapper>
     </section>
   )
